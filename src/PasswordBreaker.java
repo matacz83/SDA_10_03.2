99990f5dcddfe1c4e26b84e1f662d4008ba0e53e
matacz <matacz@gmail.com>
2019-03-10 15:52:48 +0100

    final na dziś

--- src/PasswordBreaker.java
+++ src/PasswordBreaker.java
@@ -3,23 +3,30 @@ public class PasswordBreaker {
     private final static String SECRET = "cbac";
 
     private final static char[] DICTIONARY = new char[]{'a', 'b', 'c'};
+    private static boolean found = false;
 
     private static boolean checkPassword(String input) {
+        System.out.println(input);
         return input.equals(SECRET);
     }
 
-    private static void bruteForce(String input, int n) {
-        if (n == 0) {
-            if (checkPassword(input)) {
-                System.out.println("Znalazlem hasło: " + input);
-            }
+    private static void bruteForce(String input, final int n) {
+
+        if (checkPassword(input)) {
+            found = true;
+            System.out.println(input + " <- To jest szukane hasło!");
         } else {
-            for (char character : DICTIONARY) {
-                String newInput = input + character;
-                bruteForce(newInput, n-1);
+            if (n != 0) {
+                for (char character : DICTIONARY) {
+                    if (!found) {
+                        String newInput = input + character;
+                        bruteForce(newInput, n - 1);
+                    }
+                }
             }
         }
     }
+
     public static void main(String[] args) {
         bruteForce("", 4);
     }
